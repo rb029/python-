3fc335302a15925a41e23f833edfc70284498d61
rb029 <148209628+rb029@users.noreply.github.com>
2024-07-19 18:14:55 +0530

    Add files via upload

---
+++ question5.py
@@ -0,0 +1,9 @@
+number = [12 , 75 , 150 , 180 , 145 , 525 , 50]
+for item in number:
+    if item > 500:
+        break
+    elif item > 150:
+        continue
+    elif item % 5 == 0:
+        print(item)
+
