cc30c2e8c4500f935db2fbe3e7309afd9f974855
rb029 <ramanbishnoi322@gmail.com>
2024-07-19 18:25:32 +0530

    1-6

---
+++ question3.py
@@ -0,0 +1,6 @@
+s = 0
+n = int(input("Enter Number:"))
+for i in range(1,n+1,1):
+    s=s+i
+print("\n")
+print("Sum is :",s)
---
+++ question4.py
@@ -0,0 +1,4 @@
+n = 2
+for i in range (1,11,1):
+    p = n*i
+    print(p)
